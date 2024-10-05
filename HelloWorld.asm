b5f66de1387f1a968738ca7d7f69514879686152
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-10-05 20:34:03 +0500

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,21 @@
+.model small
+.stack 100h
+
+.data
+    hello db 'Hello World', 13, 10, '$' ; String to print <Hello World with CRLF>
+
+.code
+main proc
+    mov ax, @data        ; Initialize data segment
+    mov ds, ax
+
+    ; Print Hello World
+    lea dx, hello        ; Load the address of hello into DX
+    mov ah, 09h          ; Set function for printing a string
+    int 21h              ; Call DOS interrupt
+
+    ; Terminate program
+    mov ax, 4C00h        ; Terminate program
+    int 21h              ; Call DOS interrupt
+main endp
+end main
